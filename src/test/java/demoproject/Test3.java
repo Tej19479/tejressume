8fc68b9dcb619b733752aa863cdafc1a2341eb7a
QSP <QSP@QSP>
2021-09-28 18:21:40 +0530

    hi

---
+++ src/test/java/demoproject/Test3.java
@@ -0,0 +1,12 @@
+package demoproject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello API",true);
+	}
+
+}
---
+++ src/test/java/demoproject/Test4.java
@@ -0,0 +1,12 @@
+package demoproject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello Appium",true);
+	}
+
+}
