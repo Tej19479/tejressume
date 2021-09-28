9c9f65aae227277fcf7626191024c4f2ca4da560
QSP <QSP@QSP>
2021-09-28 18:06:09 +0530

    Hi

---
+++ src/test/java/demoproject/Test1.java
@@ -0,0 +1,12 @@
+package demoproject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello Selenium",true);
+	}
+
+}
